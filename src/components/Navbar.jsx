7b63882a0aa8e9cae189a5ac928460b1a9b117c4
Kostia06 <ilnkostia@gmail.com>
2025-02-15 17:35:47 -0700

    show and hide navbar and animation for items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { CiMap } from 'react-icons/ci';
 import { CiSearch } from 'react-icons/ci';
 import { CiSettings } from 'react-icons/ci';
 import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 const links = [
     { name: 'map', href: '/map', icon: <CiMap /> },
@@ -13,26 +15,58 @@ const links = [
 
 export default function NavBar() {
     const pathname = usePathname();
+    const showNavbar = useNavbarVisibilityOnScroll();
+    const isHidden = showNavbar ? '' : 'translate-y-40';
     return (
-        <div className="fixed bottom-0 flex items-center justify-evenly border-t-2 w-full p-4">
+        <div
+            className={`fixed bottom-0 flex items-center justify-evenly border-t-2 w-full smooth h-20 ${isHidden}`}
+        >
             {links.map((link) => (
-                <Link key={link.href} currentPath={pathname} link={link} />
+                <LinkPage key={link.href} currentPath={pathname} link={link} />
             ))}
         </div>
     );
 }
 
-const Link = ({ currentPath, link }) => {
+const LinkPage = ({ currentPath, link }) => {
+    const pathname = usePathname();
     const isActive = currentPath === link.href;
-    const isActiveClass = isActive ? '-translate-y-10' : '';
+    const isActiveClass = isActive
+        ? '-translate-y-8 bg-red-200 rounded-full'
+        : '';
 
     return (
-        <a
+        <Link
             key={link.href}
             href={link.href}
-            className={`relative p-2 text-3xl ${isActiveClass}`}
+            className={`relative p-4 text-3xl smooth font-bold ${isActiveClass}`}
         >
             {link.icon}
-        </a>
+        </Link>
     );
+};
+
+const useNavbarVisibilityOnScroll = () => {
+    const [showNavbar, setShowNavbar] = useState(true); // Default: Navbar is visible
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // If the user scrolls down, hide the Navbar
+            if (window.scrollY > 0) {
+                setShowNavbar(false);
+            } else {
+                setShowNavbar(true); // If at the top, show the Navbar
+            }
+        };
+
+        // Listen for scroll events
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup event listener when component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // Empty dependency array ensures this runs only once on mount
+
+    return showNavbar; // Return the state to control Navbar visibility
 };
