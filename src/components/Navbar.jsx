701ff2b2c8a651143368325a68d23f571b163755
Kostia06 <ilnkostia@gmail.com>
2025-02-15 17:07:33 -0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 'use client';
 
+import { CiMap } from 'react-icons/ci';
+import { CiSearch } from 'react-icons/ci';
+import { CiSettings } from 'react-icons/ci';
+import { usePathname } from 'next/navigation';
+
 const links = [
-    { name: 'home', href: '/' },
-    { name: 'info', href: '/info' },
-    { name: 'about', href: '/about' },
+    { name: 'map', href: '/map', icon: <CiMap /> },
+    { name: 'explore', href: '/explore', icon: <CiSearch /> },
+    { name: 'profile', href: '/profile', icon: <CiSettings /> },
 ];
 
 export default function NavBar() {
+    const pathname = usePathname();
     return (
-        <div className="fixed bottom-0 left-0 flex items-center justify-center bg-red-200 text-2xl">
-            Hello World
+        <div className="fixed bottom-0 flex items-center justify-evenly border-t-2 w-full p-4">
+            {links.map((link) => (
+                <Link key={link.href} currentPath={pathname} link={link} />
+            ))}
         </div>
     );
 }
+
+const Link = ({ currentPath, link }) => {
+    const isActive = currentPath === link.href;
+    const isActiveClass = isActive ? '-translate-y-10' : '';
+
+    return (
+        <a
+            key={link.href}
+            href={link.href}
+            className={`relative p-2 text-3xl ${isActiveClass}`}
+        >
+            {link.icon}
+        </a>
+    );
+};
