5adf83c7e5cabe87f749ce603964dfae09d81ea4
KlausMikhaelson <satyamsingh5076@gmail.com>
2025-02-16 05:04:52 -0700

    started working on search

--- src/components/QuickOptions.jsx
+++ src/components/QuickOptions.jsx
@@ -1,10 +1,10 @@
 import Search from '@/components/Search';
 import CreatePost from '@/components/CreatePost';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function QuickOptions() {
     const [search, setSearch] = useState("");
-    
+
     return (
         <div className="fixed top-0 left-0 m-4 flex flex-col items-start justify-center space-y-4">
             <Search search={search} setSearch={setSearch}/>
